1f4cd3a716b6426dec1b55d6a0138f2227e64a1d
MeanaOrl <77915799+MeanaOrl@users.noreply.github.com>
2022-10-13 17:31:38 +0300

    Update La8.java

--- La8.java
+++ La8.java
@@ -3,9 +3,16 @@ import java.util.Scanner;
 public class La8 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        int cnt = 1;
         int a = s.nextInt();
+        System.out.println(cnt);
         int b = s.nextInt();
-        int cnt = 0;
+        if (a >= b) {
+        }
+        else {
+            cnt++;
+        }
+        System.out.println(cnt);
         try {
             while (true) {
                 int c = s.nextInt();
